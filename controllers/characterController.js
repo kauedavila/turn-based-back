ab5d004d83d8889e6cb10d6cd9113bd39863d760
kauedavila <kaueourives2@gmail.com>
2024-03-17 10:46:17 -0300

    feat: character getById

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -1,4 +1,4 @@
-const { Character } = require("../models/Character");
+const { Character: CharacterModel } = require("../models/Character");
 
 const characterController = {
   create: async (req, res) => {
@@ -7,8 +7,7 @@ const characterController = {
         name: req.body.name,
         level: req.body.level,
       };
-      const response = await Character.create(character);
-
+      const response = await CharacterModel.create(character);
       res.status(201).json({ response, msg: "Character created successfully" });
     } catch (error) {
       res.status(400).send(error);
@@ -17,10 +16,20 @@ const characterController = {
 
   getAll: async (req, res) => {
     try {
-      const characters = await Character.find();
+      const characters = await CharacterModel.find();
       res.status(200).json(characters);
     } catch (error) {
       res.status(400).json({ error, msg: "Error while trying to get characters" });
+    }
+  },
+
+  getById: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const character = await CharacterModel.findById(id);
+      res.status(200).json(character);
+    } catch (error) {
+      res.status(400).json({ error, msg: "Error while trying to get character" });
     }
   },
 };
--- routes/character.js
+++ routes/character.js
@@ -4,5 +4,6 @@ const characterController = require("../controllers/characterController");
 
 router.route("/characters").post((req, res) => characterController.create(req, res));
 router.route("/characters").get((req, res) => characterController.getAll(req, res));
+router.route("/characters/:id").get((req, res) => characterController.getById(req, res));
 
 module.exports = router;
