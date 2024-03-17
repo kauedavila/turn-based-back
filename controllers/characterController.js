07badbb679d570c12ef1bdaef1fd9168df9e983a
kauedavila <kaueourives2@gmail.com>
2024-03-17 11:35:43 -0300

    feat: update & delete character by Id

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -37,6 +37,42 @@ const characterController = {
       res.status(400).json({ error, msg: "Error while trying to get character" });
     }
   },
+
+  deleteById: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const character = await CharacterModel.findById(id);
+
+      if (!character) {
+        return res.status(404).json({ msg: "Character not found" });
+      }
+
+      const deleteCharacter = await CharacterModel.findByIdAndDelete(id);
+
+      if (!deleteCharacter) {
+        return res.status(400).json({ msg: "Error while trying to delete character" });
+      }
+
+      res.status(200).json({ deleteCharacter, msg: "Character deleted successfully" });
+    } catch (error) {
+      res.status(400).json({ error, msg: "Error while trying to delete character" });
+    }
+  },
+
+  updateById: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const character = {
+        name: req.body.name,
+        level: req.body.level,
+      };
+      const updatedCharacter = await CharacterModel.findByIdAndUpdate(id, character);
+
+      res.status(200).json(character);
+    } catch (error) {
+      res.status(400).json({ error, msg: "Error while trying to get character" });
+    }
+  },
 };
 
 module.exports = characterController;
--- routes/character.js
+++ routes/character.js
@@ -2,8 +2,17 @@ const router = require("express").Router();
 
 const characterController = require("../controllers/characterController");
 
+//POST /characters
 router.route("/characters").post((req, res) => characterController.create(req, res));
+
+//GET /characters
 router.route("/characters").get((req, res) => characterController.getAll(req, res));
 router.route("/characters/:id").get((req, res) => characterController.getById(req, res));
+
+//DELETE /characters/:id
+router.route("/characters/:id").delete((req, res) => characterController.deleteById(req, res));
+
+//PUT /characters/:id
+router.route("/characters/:id").put((req, res) => characterController.updateById(req, res));
 
 module.exports = router;
