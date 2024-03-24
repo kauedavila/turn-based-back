6222f46570be5aec645d7d57871c401fc8cb882d
Kaue <kaueourives2@gmail.com>
2024-03-24 15:00:04 -0300

    feat: utilizando bcrypt ao criar usuário

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const { Schema } = mongoose;
 
@@ -27,6 +28,14 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
